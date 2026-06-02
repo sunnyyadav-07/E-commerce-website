02e630fdc31a91dd39ed29c3acf8db26072b2014
Sunny Yadav <yadavsunny1916@gmail.com>
2026-06-02 19:14:20 +0530

    create model for cart api

--- Backend/src/models/cart.model.js
+++ Backend/src/models/cart.model.js
@@ -1,6 +1,35 @@
 import mongoose from "mongoose";
 
-const cartSchema = new mongoose.Schema({});
+const cartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+      unique: true,
+    },
+    items: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "product",
+          required: true,
+        },
+        variant: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          min: 1,
+          default: 1,
+          required: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true },
+);
 
 const cartModel = mongoose.model("cart", cartSchema);
 export default cartModel;
