1d397434f767bd032e99f23273324e381ab6dd30
Sunny Yadav <yadavsunny1916@gmail.com>
2026-06-15 22:16:49 +0530

    create APIs to get all items from wishlist and to remove an item from wishlist

--- Backend/src/controllers/wishlist.controller.js
+++ Backend/src/controllers/wishlist.controller.js
@@ -1,4 +1,20 @@
 import wishListModel from "../models/wishlist.model.js";
+async function getUserWishList(userId, populate = false) {
+  let query = wishListModel.findOne({
+    userId,
+  });
+  if (populate) {
+    query = query.populate("products.productId");
+  }
+  const userWishList = await query;
+  if (!userWishList) {
+    return res.status(404).json({
+      success: false,
+      message: "Wishlist not found",
+    });
+  }
+  return userWishList;
+}
 
 export async function addToWishList(req, res) {
   try {
@@ -44,6 +60,91 @@ export async function addToWishList(req, res) {
     });
   } catch (error) {
     console.log("error in add to wishlist API", error);
+    return res.status(500).json({
+      success: false,
+      message: "Server error",
+      error,
+    });
+  }
+}
+
+export async function getAllWishListItemsController(req, res) {
+  try {
+    const userId = req.user._id;
+    const userWishList = await getUserWishList(userId, true);
+    if (userWishList.products.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "Do not have any item in wishlist",
+        wishlist: userWishList,
+      });
+    }
+    const formattedWishListItems = userWishList.products.map((item) => {
+      const variant = item.productId.variants.id(item.variantId);
+      return {
+        productId: item.productId._id,
+        title: item.productId.title,
+        brand: item.productId.brand,
+        category: item.productId.category,
+        subCategory: item.productId.subCategory,
+        price: variant.price.amount,
+        image: variant.images[0],
+        variantId: variant._id,
+      };
+    });
+    return res.status(200).json({
+      success: true,
+      message: "Wishlist fetched successfully",
+      wishlist: formattedWishListItems,
+    });
+  } catch (error) {
+    console.log("Error in fetching all wishlist items", error);
+    return res.status(500).json({
+      success: false,
+      message: "Server error",
+      error,
+    });
+  }
+}
+
+export async function removeItemFromWishListController(req, res) {
+  try {
+    const { productId, variantId } = req.params;
+    const userId = req.user._id;
+    if (!variantId || !productId) {
+      return res.status(400).json({
+        success: false,
+        message: "Params are required",
+      });
+    }
+    const userWishList = await getUserWishList(userId);
+    if (userWishList.products.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "Do not have any item in wishlist",
+        wishlist: userWishList,
+      });
+    }
+    const itemToBeDeleted = userWishList.products.find(
+      (item) =>
+        item.productId.equals(productId) && item.variantId.equals(variantId),
+    );
+    if (!itemToBeDeleted) {
+      return res.status(404).json({
+        success: false,
+        message: "Item not found in wishlist",
+        wishlist: userWishList,
+      });
+    }
+    itemToBeDeleted.deleteOne();
+    await userWishList.save();
+    return res.status(200).json({
+      success: true,
+      message: "Item deleted from wishlist succussfully",
+      wishlist: userWishList,
+    });
+  } catch (error) {
+    console.log("Error in removing an item from wishlist", error);
     return res.status(500).json({
       success: false,
       message: "Server error",
--- Backend/src/routes/wishlist.route.js
+++ Backend/src/routes/wishlist.route.js
@@ -1,7 +1,17 @@
 import { Router } from "express";
-import { addToWishList } from "../controllers/wishlist.controller.js";
+import {
+  addToWishList,
+  getAllWishListItemsController,
+  removeItemFromWishListController,
+} from "../controllers/wishlist.controller.js";
 import { authUser } from "../middlewares/auth.middleware.js";
 import { wishListValidator } from "../validators/wishlist.validator.js";
 const wishListRouter = Router();
 wishListRouter.post("/add", authUser, wishListValidator, addToWishList);
+wishListRouter.get("/all-items", authUser, getAllWishListItemsController);
+wishListRouter.delete(
+  "/item/:productId/:variantId",
+  authUser,
+  removeItemFromWishListController,
+);
 export default wishListRouter;
