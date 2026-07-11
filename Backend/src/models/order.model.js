d0d1705770b42e02ba1f5bb8bbc5ce53c439f035
Sunny Yadav <yadavsunny1916@gmail.com>
2026-07-11 21:44:26 +0530

    create order model

--- Backend/src/models/order.model.js
+++ Backend/src/models/order.model.js
@@ -1,5 +1,57 @@
 import mongoose from "mongoose";
-const orderSchema = new mongoose.Schema({});
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "user",
+    },
+    items: [
+      {
+        seller: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: "user",
+        },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: "product",
+        },
+        variant: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+          min: [0, "Price can not be negative"],
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: [1, "Quantity must be at least 1 "],
+        },
+        itemStatus: {
+          type: String,
+          enum: ["pending", "processing", "shipped", "delivered", "cancelled"],
+          default: "pending",
+        },
+      },
+    ],
+    orderStatus: {
+      type: String,
+      enum: ["pending", "placed", "cancelled"],
+      default: "pending",
+    },
+    totalAmount: {
+      type: Number,
+      required: true,
+      min: [0, "Total amount can not be negative"],
+    },
+  },
+  { timestamps: true },
+);
 
 const orderModel = mongoose.model("order", orderSchema);
 export default orderModel;
