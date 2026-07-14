8eaa22e75b406ad0ee73ba69c0895bcce28d2176
Sunny Yadav <yadavsunny1916@gmail.com>
2026-07-14 21:51:43 +0530

    start creating orders APIs

--- Backend/src/controllers/order.controller.js
+++ Backend/src/controllers/order.controller.js
@@ -1,1 +1,9 @@
-export async function createOrderController(req, res) {}
+export async function createOrderController(req, res, next) {
+  try {
+    const userId = req.user._id;
+    const items = req.body;
+    
+  } catch (error) {
+    next(error);
+  }
+}
